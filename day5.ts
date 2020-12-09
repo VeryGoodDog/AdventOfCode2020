3ac348e860b67b0b4b04e75b87dc0024366e4be2
VeryGoodDog <alex.h@me.com>
2020-12-09 13:14:05 -0500

    day 5! (part two doesnt work and i have no idea why)

---
+++ day5.ts
@@ -0,0 +1,50 @@
+/// <reference path="./types.d.ts"/>
+const POSITIONS = Deno.args[0].split('\n');
+console.log('Part 1:');
+{
+	let maxSeatId = 0;
+	POSITIONS.forEach(pos => {
+		const fbPos = pos.substring(0, 7)
+			.replaceAll('F', '0')
+			.replaceAll('B', '1');
+		const numfbPos = parseInt(fbPos, 2);
+		const lrPos = pos.substring(7)
+			.replaceAll('R', '0')
+			.replaceAll('L', '1');
+		const numlrPos = parseInt(lrPos, 2);
+		const seatId = numfbPos * 8 + numlrPos;
+		maxSeatId = maxSeatId < seatId ? seatId : maxSeatId;
+	});
+	console.log(maxSeatId);
+}
+
+console.log('Part 2:');
+{
+	const posArray = POSITIONS.map(pos => {
+		const fbPos = pos.substring(0, 8)
+			.replaceAll('F', '0')
+			.replaceAll('B', '1');
+		const numfbPos = Number.parseInt(fbPos, 2);
+		const lrPos = pos.substring(7)
+			.replaceAll('R', '0')
+			.replaceAll('L', '1');
+		const numlrPos = Number.parseInt(lrPos, 2);
+		return numfbPos * 8 + numlrPos;
+	});
+	posArray.sort((a, b) => a - b);
+	let seat = '';
+	for (var i = 1; i < posArray.length; i++) {
+		const delta = posArray[i] - posArray[i - 1];
+		if (delta == 2) {
+			seat = (posArray[i] - 1).toString(2);
+			break;
+		}
+	}
+	const seatTop = seat.substring(0, 7)
+		.replaceAll('0', 'F')
+		.replaceAll('1', 'B');
+	const seatLow = seat.substring(7)
+		.replaceAll('0', 'R')
+		.replaceAll('1', 'L');
+	console.log(seatTop + seatLow);
+}
